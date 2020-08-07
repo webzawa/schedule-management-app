71f2d810a35798233942e1e846ca74d0e1f355b3
hinnahinna <m.serizawa2064@gmail.com>
2020-08-07 09:18:47 +0900

    fixed css

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,13 +4,4 @@ class PagesController < ApplicationController
   # before_action :authenticate_user!
 
   def home; end
-
-  def beforemonth
-    
-  end
-
-  def nextmonth
-    # @beginningday = @beginningday >> 1
-    # @beginningday = @beginningday + '0000/01/00'
-  end
 end
