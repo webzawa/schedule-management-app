d587a6d39f96fa3808cf3a598bbf2e5eccbb659c
hinnahinna <m.serizawa2064@gmail.com>
2020-08-21 15:00:17 +0900

    adjust css

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# ユーザ作成
 5.times do |n|
   User.create!(
     :username => "ユーザ#{n + 1}",
@@ -10,18 +11,63 @@
   )
 end
 
+# 店舗作成
 3.times do |n|
   Store.create!(
     :storename => "ストア#{n + 1}店"
   )
 end
 
-30.times do |n|
+# シフト作成
+31.times do |n|
   Schedule.create!(
     :request_day => "2020-08-#{n + 1}",
     :request_timezone => 'A',
     :approved => true,
     :store_id => 1,
     :user_id => 1
+  )
+end
+
+31.times do |n|
+  Schedule.create!(
+    :request_day => "2020-08-#{n + 1}",
+    :request_timezone => 'AB',
+    :approved => true,
+    :store_id => 1,
+    :user_id => 2
+  )
+end
+
+31.times do |n|
+  Schedule.create!(
+    :request_day => "2020-08-#{n + 1}",
+    :request_start_time => 16,
+    :request_end_time => 16,
+    :request_timezone => 'D',
+    :approved => true,
+    :store_id => 1,
+    :user_id => 3
+  )
+end
+
+31.times do |n|
+  Schedule.create!(
+    :request_day => "2020-08-#{n + 1}",
+    :request_timezone => 'E1',
+    :approved => true,
+    :store_id => 1,
+    :user_id => 4
+  )
+end
+
+31.times do |n|
+  Schedule.create!(
+    :request_day => "2020-08-#{n + 1}",
+    :request_start_time => 12,
+    :request_end_time => 18,
+    :approved => true,
+    :store_id => 1,
+    :user_id => 5
   )
 end
