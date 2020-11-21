f13baf6f8f4e761188541a071f9f28dfd8243243
webzawa <m.serizawa2064@gmail.com>
2020-11-22 07:40:03 +0900

    configlation order to calender Users

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -175,7 +175,8 @@ class SchedulesController < ApplicationController
 
       # 勤務時間帯順に並び替え
       @users_and_schedules_search.sorts = 'duty_hours asc' if @users_and_schedules_search.sorts.blank?
-      @users_and_schedules = @users_and_schedules_search.result(:distinct => true).includes(:schedules) # schedulesをincludesしないと検索がうまくいかない
+      # 下記部分はschedulesをincludesしないと検索がうまくいかない。さらに、User作成順で昇順ソートするよう設定。
+      @users_and_schedules = @users_and_schedules_search.result(:distinct => true).order(:created_at => 'ASC').includes(:schedules)
 
       # 検索対象のレコードがない場合
       if @users_and_schedules.blank?
