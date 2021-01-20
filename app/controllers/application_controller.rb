3898d686224187581e8dd0189f7cb566614181eb
webzawa <m.serizawa2064@gmail.com>
2021-01-20 19:56:42 +0900

    add join_date setting & adjust callender

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = %i[username comment work_store duty_hours email password password_confirmation]
+    added_attrs = %i[username comment work_store duty_hours join_date email password password_confirmation]
     devise_parameter_sanitizer.permit :sign_up, :keys => added_attrs
     devise_parameter_sanitizer.permit :account_update, :keys => added_attrs
     devise_parameter_sanitizer.permit :sign_in, :keys => added_attrs
