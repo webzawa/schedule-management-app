97d6f04c59c48e3732cb3f3c9dd0c80d71e64f0b
webzawa <m.serizawa2064@gmail.com>
2021-01-25 18:17:52 +0900

    adjust production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { :host => 'https://www.schedulemanagement.work/' }
+  config.action_mailer.default_url_options = { :host => 'https://limitbreakschedule.herokuapp.com/' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
